9d185205647d1d199a1c9572b6353138c8ae3684
45gfg9 <mc45@qq.com>
2020-09-23 13:49:28 +0800

    "Data Structure"

--- lib/FixedStack/FixedStack.cpp
+++ lib/FixedStack/FixedStack.cpp
@@ -1,1 +1,46 @@
 #include "FixedStack.h"
+
+template <typename E>
+FixedStack<E>::FixedStack(size_t size) : _size(size), _tos(0)
+{
+    _elementArray = new E[size];
+}
+
+template <typename E>
+FixedStack<E>::~FixedStack()
+{
+    delete[] _elementArray;
+    _elementArray = nullptr;
+}
+
+template <typename E>
+bool FixedStack<E>::push(const E &e)
+{
+    if (isFull())
+        return false;
+
+    _elementArray[_tos++] = e;
+    return true;
+}
+
+template <typename E>
+bool FixedStack<E>::pop(E &e)
+{
+    if (isEmpty())
+        return false;
+
+    e = _elementArray[--_tos];
+    return true;
+}
+
+template <typename E>
+inline bool FixedStack<E>::isEmpty() const
+{
+    return _tos == 0;
+}
+
+template <typename E>
+inline bool FixedStack<E>::isFull() const
+{
+    return _tos == _size;
+}
--- lib/FixedStack/FixedStack.h
+++ lib/FixedStack/FixedStack.h
@@ -1,12 +1,26 @@
 #ifndef __FIXEDSTACK__
 #define __FIXEDSTACK__
 
+#include <stddef.h>
+
+// yys: template <typename E, int size>
 template <typename E>
 class FixedStack
 {
+    const size_t _size;
+
+    E *_elementArray;
+    size_t _tos;
+
 public:
-    FixedStack(int size);
+    FixedStack(size_t size);
     ~FixedStack();
+
+    bool push(const E &e);
+    bool pop(E &e);
+
+    bool isEmpty() const;
+    bool isFull() const;
 };
 
 #endif
