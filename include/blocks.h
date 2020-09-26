e1e89f5bc0dd11d9ab84655e95ccbdf857e99ee6
45gfg9 <mc45@qq.com>
2020-09-26 10:08:54 +0800

    Comments on blocks

--- include/blocks.h
+++ include/blocks.h
@@ -28,30 +28,98 @@ public:
     void paint() const;
 };
 
+/*
+
+    +---+
+    | 1 |
++---+---+---+
+| 2 | 3 | 4 |
++---+---+---+
+
+*/
+// T-Shaped Block
 class TBlock : public Block
 {
 };
 
+/*
+
++---+---+---+---+
+| 1 | 2 | 3 | 4 |
++---+---+---+---+
+
+*/
+// I-Shaped Block
 class IBlock : public Block
 {
 };
 
+/*
+
+        +---+
+        | 4 |
++---+---+---+
+| 1 | 2 | 3 |
++---+---+---+
+
+*/
+// L-Shaped Block
 class LBlock : public Block
 {
 };
 
+/*
+
++---+---+
+| 1 | 2 |
++---+---+---+
+    | 3 | 4 |
+    +---+---+
+
+*/
+// Z-Shaped Block
 class ZBlock : public Block
 {
 };
 
+/*
+
++---+
+| 4 |
++---+---+---+
+| 1 | 2 | 3 |
++---+---+---+
+
+*/
+// Flipped L-Shaped Block
 class FlipLBlock : public Block
 {
 };
 
+/*
+
+    +---+---+
+    | 1 | 2 |
++---+---+---+
+| 3 | 4 |
++---+---+
+
+*/
+// Flipped Z-Shaped Block
 class FlipZBlock : public Block
 {
 };
 
+/*
+
++---+---+
+| 1 | 2 |
++---+---+
+| 3 | 4 |
++---+---+
+
+*/
+// Square Block
 class SqBlock : public Block
 {
 };
