1480e70c15cf67942e3155efefdea66c452a260d
45gfg9 <45gfg9@45gfg9.net>
2024-02-13 02:11:56 +0800

    Replace input switch with if-else

--- src/input.cpp
+++ src/input.cpp
@@ -3,48 +3,34 @@
 
 input_t getInput() {
   // tolower
-  int ch = Serial.read();
-  if (ch == '\x1b' && Serial.available()) {
+  int c = Serial.read();
+  if (c == '\x1b' && Serial.available()) {
     // not just a plain ESC
-    ch = Serial.read();
+    c = Serial.read();
 
     // arrow sequences
-    if (ch == '[') {
-      ch = 0x80 | Serial.read();
+    if (c == '[') {
+      c = 0x80 | Serial.read();
     }
   }
-  switch (ch) {
-    // case 0x80 | 'A':
-    case ' ':
-    case '8':
-      return TR_IN_LOCK;
-    case 0x80 | 'B':
-    case '2':
-      return TR_IN_DOWN;
-    case 0x80 | 'C':
-    case '6':
-      return TR_IN_RIGHT;
-    case 0x80 | 'D':
-    case '4':
-      return TR_IN_LEFT;
-    case '\x1b':
-      return TR_IN_PAUSE;
-    case 'z':
-    case 'Z':
-    case '3':
-    case '7':
-      return TR_IN_ROT_CCW;
-    case 'x':
-    case 'X':
-    case '1':
-    case '5':
-    case '9':
-      return TR_IN_ROT_CW;
-    case 'c':
-    case 'C':
-    case '0':
-      return TR_IN_HOLD;
-  }
 
-  return TR_IN_NONE;
+  if (c == (0x80 | 'A') || c == ' ' || c == '8') {
+    return TR_IN_LOCK;
+  } else if (c == (0x80 | 'B') || c == '2') {
+    return TR_IN_DOWN;
+  } else if (c == (0x80 | 'C') || c == '6') {
+    return TR_IN_RIGHT;
+  } else if (c == (0x80 | 'D') || c == '4') {
+    return TR_IN_LEFT;
+  } else if (c == '\x1b') {
+    return TR_IN_PAUSE;
+  } else if (c == 'z' || c == 'Z' || c == '3' || c == '7') {
+    return TR_IN_ROT_CCW;
+  } else if (c == 'x' || c == 'X' || c == '1' || c == '5' || c == '9') {
+    return TR_IN_ROT_CW;
+  } else if (c == 'c' || c == 'C' || c == '0') {
+    return TR_IN_HOLD;
+  } else {
+    return TR_IN_NONE;
+  }
 }
